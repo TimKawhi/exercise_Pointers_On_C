2b9f535a5eb34f04cb6a2a8a6c1c190e0c22d5c8
cooper_liu <1579651923@qq.com>
2018-08-12 09:55:13 +0800

    Create dll_ins2.c
    将dll_ins1.c中的if结构进行了优化，简化了代码

---
+++ chapter12/dll_ins2.c
@@ -0,0 +1,66 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"dll_node.h"
+
+int
+dll_insert( Node *rootp, int value )
+{
+    Node *this;
+    Node *next;
+    Node *newnode;
+
+    /*
+    **查看value是否在已经在链表中，如果是就返回
+    **否则为其创造一个新的节点--newone
+    **"this"将指向新节点之前的那个节点
+    **"next"将指向新节点之后的那个节点
+    *
+    */
+    for( this = rootp; (next = this->fwd) != NULL; this = next ){
+        if( next->value == value )
+            return 0;
+        if( next->value > value )
+            break;
+    }
+
+    newnode = (Node *)malloc( sizeof( Node ) );
+    if( newnode == NULL )
+        return -1;
+    newnode->value = value;
+
+    /*
+    **把新值添加到链表中
+    */
+    if( next != NULL ) {
+    /*
+    **情况1 或情况2：并非位于链表尾部
+    */
+        newnode->fwd = next;
+        if( this != rootp ){ /* 情况1：并非位于链表的起始位置*/
+            this->fwd = newnode;
+            newnode->bwd = this;
+        }
+        else {               /* 情况2：位于链表的起始位置*/
+            rootp->fwd = newnode;
+            newnode->bwd = NULL;
+        }
+        next->bwd = newnode;
+    }
+    else {
+    /*
+    **情况3或情况4：位于链表的尾部
+    */
+        newnode->fwd = NULL;
+        if( this != rootp ) {   /* 情况3：不位于链表的起始位置*/
+            this->fwd = newnode;
+            newnode->bwd = this; 
+        }
+        else {                  /* 情况4：位于链表的起始位置*/
+            rootp->fwd = newnode;
+            newnode->bwd = NULL;
+        }
+        rootp->bwd = newnode;    
+    }
+
+    return 1;
+}
